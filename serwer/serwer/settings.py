a632ba3a381fd62de07b6945735261c00f9499b2
Sam355 <samx355@gmail.com>
2021-04-08 23:08:19 +0200

    Dodano połączenie z bazą danych oraz API Coinpaprika

--- serwer/serwer/settings.py
+++ serwer/serwer/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'serwer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cryptoanalyzer',
+        'USER': 'postgres',
+        'PASSWORD': 'pA4g8u4dy53rhSG',
+        'HOST': 'cryptoanalyzer.cojze30cduke.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
