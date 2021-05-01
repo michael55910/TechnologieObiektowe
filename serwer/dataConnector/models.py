882fc0a61ade219ed6b9198ec29b85ea42019aa0
Sam355 <samx355@gmail.com>
2021-05-01 19:12:24 +0200

    Dodanie cyklicznych wywołań funkcji

--- serwer/dataConnector/models.py
+++ serwer/dataConnector/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 from coinpaprika import client as Coinpaprika
 from binance.client import Client
+import schedule, time
 
 BinanceClient = Client("y4IYuRu7rcBuBRxbT57hdrUE12UpvMZdzJOdqPGrdS4jTU2oi9onl4bNxvZ6qMEj",
                        "BZ3y4qZ8019zzU6F2hv4WLbDnpeIQMBIuefMHxb1qPeloUayZUbOv1Che8Fzy34C")
 CoinpaprikaClient = Coinpaprika.Client()
 
 available_coins = CoinpaprikaClient.coins()
-
-exchange_rates = BinanceClient.get_ticker()
-
-candles = BinanceClient.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
-
-print(candles)
 
 
 class Cryptocurrency(models.Model):
@@ -38,7 +33,7 @@ class Cryptocurrency(models.Model):
 
 
 class Rate(models.Model):
-    symbol = models.CharField(max_length=20, blank=False)
+    symbol = models.CharField(max_length=20, primary_key=True, blank=False)
     price_change = models.FloatField(blank=False)
     price_change_percent = models.FloatField(blank=False)
     weighted_avg_price = models.FloatField(blank=False)
@@ -87,32 +82,51 @@ for x in available_coins:
                               is_new=x['is_new'], is_active=x['is_active'], type=x['type'])
     coins_list.append(new_coin)
 
+
 # Cryptocurrency.objects.bulk_create(coins_list)
 
-exchange_list = []
+def update_rates():
+    exchange_rates = BinanceClient.get_ticker()
 
-for x in exchange_rates:
-    new_rate = Rate(symbol=x['symbol'], price_change=x['priceChange'],
-                    price_change_percent=x['priceChangePercent'], weighted_avg_price=x['weightedAvgPrice'],
-                    prev_close_price=x['prevClosePrice'], last_price=x['lastPrice'], last_qty=x['lastQty'],
-                    bid_price=x['bidPrice'],
-                    ask_price=x['askPrice'], open_price=x['openPrice'], high_price=x['highPrice'],
-                    low_price=x['lowPrice'], volume=x['volume'],
-                    quote_volume=x['quoteVolume'], open_time=x['openTime'], close_time=x['closeTime'],
-                    first_id=x['firstId'], last_id=x['lastId'],
-                    count=x['count'])
-    exchange_list.append(new_rate)
+    exchange_list = []
 
-# Rate.objects.bulk_create(exchange_list)
+    for x in exchange_rates:
+        new_rate = Rate(symbol=x['symbol'], price_change=x['priceChange'],
+                        price_change_percent=x['priceChangePercent'], weighted_avg_price=x['weightedAvgPrice'],
+                        prev_close_price=x['prevClosePrice'], last_price=x['lastPrice'], last_qty=x['lastQty'],
+                        bid_price=x['bidPrice'], ask_price=x['askPrice'], open_price=x['openPrice'],
+                        high_price=x['highPrice'], low_price=x['lowPrice'], volume=x['volume'],
+                        quote_volume=x['quoteVolume'], open_time=x['openTime'], close_time=x['closeTime'],
+                        first_id=x['firstId'], last_id=x['lastId'], count=x['count'])
+        exchange_list.append(new_rate)
 
-candle_list = []
+    Rate.objects.bulk_create(exchange_list)
+    print("Exchange rates updated successfully!")
 
-for x in candles:
-    new_candle = Candle(open_time=x[0], open=x[1], high=x[2], low=x[3],
-                        close=x[4], volume=x[5], close_time=x[6],
-                        quote_asset_volume=x[7], number_of_trades=x[8],
-                        taker_buy_base_asset_volume=x[9], taker_buy_quote_asset_volume=x[10],
-                        ignore=x[11])
-    candle_list.append(new_candle)
 
-# Candle.objects.bulk_create(candle_list)
+def update_candles():
+    candles = BinanceClient.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
+
+    candle_list = []
+
+    for x in candles:
+        new_candle = Candle(open_time=x[0], open=x[1], high=x[2], low=x[3],
+                            close=x[4], volume=x[5], close_time=x[6],
+                            quote_asset_volume=x[7], number_of_trades=x[8],
+                            taker_buy_base_asset_volume=x[9], taker_buy_quote_asset_volume=x[10],
+                            ignore=x[11])
+        candle_list.append(new_candle)
+
+    Candle.objects.bulk_create(candle_list)
+    print("Candles updated successfully!")
+
+
+schedule.every(5).seconds.do(update_rates)
+schedule.every(5).seconds.do(update_candles)
+
+# schedule.every().day.at("10:00").do(update_rates)
+# schedule.every().day.at("10:00").do(update_candles)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
